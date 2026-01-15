81011c23204328449257b59acf11ac3cdd4c6e5e
Viktor226 <vanistratenko.a.5@gmail.com>
2026-01-15 09:46:25 +0200

    Add Streamlit app for student grades tracking

--- app.py
+++ app.py
@@ -1,1 +1,48 @@
+import streamlit as st
+import pandas as pd
 
+st.title("Оценки и ученици")
+
+if "uchenici" not in st.session_state:
+    st.session_state.uchenici = {
+        "Alex": 0,
+        "NIki": 0,
+        "Viktor": 0,
+        "Dimitar": 0
+    }
+
+if "grades" not in st.session_state:
+    st.session_state.grades = {
+        "6": 0,
+        "5": 0,
+        "4": 0,
+        "3": 0,
+        "2": 0
+    }
+
+st.subheader("Избери ученици и оценките им")
+
+color = st.selectbox("ученик:", list(st.session_state.uchenici.keys()))
+sport = st.selectbox("оценка:", list(st.session_state.grades.keys()))
+
+
+if st.button("Запази избора"):
+    st.session_state.uchenici[color] += 1
+    st.session_state.grades[sport] += 1
+    st.success("Изборът е записан!")
+
+st.divider()
+
+st.subheader("☑️ Резултати")
+
+st.write("ученик")
+colors_df = pd.DataFrame.from_dict(
+    st.session_state.uchenici, orient="index", columns=["Брой"]
+)
+st.bar_chart(uchenici_df)
+
+st.write("оценки")
+grades_df = pd.DataFrame.from_dict(
+    st.session_state.grades, orient="index", columns=["Брой"]
+)
+st.bar_chart(grades_df)
